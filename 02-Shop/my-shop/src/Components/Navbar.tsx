c756df0b295052af1d770712c1ac182d951e325d
mohammad vaseti <mohammadvaseti1998@gmail.com>
2026-02-24 16:54:48 +0330

    repo updated

--- 02-Shop/my-shop/src/Components/Navbar.tsx
+++ 02-Shop/my-shop/src/Components/Navbar.tsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
+import Container from "./Container";
 
 const Navbar = () => {
   return (
     <>
-      <div className="h-14 border-b shadow flex justify-between flex-row-reverse items-center">
-        <ul className="flex">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/store">Store</Link>
-          </li>
-        </ul>
-
-        <div>
-          <button>Buy</button>
-        </div>
+      <div className="h-14 border-b shadow flex items-center">
+        <Container>
+          <div className="flex justify-center  m-auto">
+            <div>
+              <ul className="flex">
+                <li className="ml-4">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="ml-4">
+                  <Link to="/store">Store</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button className="ml-5">Buy</button>
+            </div>
+          </div>
+        </Container>
       </div>
     </>
   );
